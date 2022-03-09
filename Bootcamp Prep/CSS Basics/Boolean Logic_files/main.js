6164e51f314f9e3a75971254558e3d443c484b03
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-09 14:24:48 +0400

    bootcamp prep

---
+++ Bootcamp Prep/CSS Basics/Boolean Logic_files/main.js
@@ -0,0 +1,74 @@
+// let favoriteFood = prompt("What's your favorite food");
+
+// if(favoriteFood === "pizza") {
+//     console.log("Woah! My Favorite food is pizza too!");
+// } else {
+//     console.log("That's cool. My favorite food is pizza");
+// }
+
+
+// let number = 55;
+
+// if(number == "55") {
+//     console.log("Yes!");
+// } else {
+//     console.log("No");
+// }
+
+
+// let x = 4;
+// if(x <= 5){
+//     console.log("x is less than or equal to five!");
+// } else {
+//     console.log("x is not less than or equal to five!");
+// }
+
+
+// TERNARY OPERATOR
+
+// let guess = prompt("Guess what number I'm thinking of!");
+// guess === "7" ? console.log("Correct!") : console.log("Incorrect!");
+
+
+// let number = prompt("What's your favorite number?");
+
+// if (number >= 1000) {
+//     console.log("Woah, that's a big number");
+// } else if (number >= 0) {
+//     console.log("That's a cool number.");
+// } else {
+//     console.log("Negative Numbers?! That's just bananas.");
+// }
+
+
+// let feeling = prompt("How are you felling today?").toLowerCase();
+
+// switch(feeling) {
+//     case "happy":
+//         console.log("Awesome, I'm feeling happy too!");
+//         break;
+//     case "sad":
+//         console.log("That's too bad, I hope you fell better soon.");
+//         break;
+//     case "hungry":
+//         console.log("Me too, let's go eat some pizza!");
+//         break;
+//     default: 
+//         console.log("I see. Thanks for sharing!")
+// }
+
+// let num = prompt("Please enter a whole number");
+// if (num % 2 === 0) {
+//     console.log("the num variable is even!");
+// } else if (num % 2 === 1) {
+//     console.log("the num variable is odd!");
+// } else {
+//     console.log("Hey! I asked for a whole number");
+// }
+
+let num = prompt()
+
+
+
+
+
---
+++ Bootcamp Prep/JS/Varaible and Primitive/main.js
@@ -0,0 +1,69 @@
+// let firstName = "Wayne";
+// let lastName = "Celestin";
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// let firstName = "Wayne";
+// console.log("Hi, " + firstName + "!");
+// console.log("How are you doing, " + firstName + "?");
+// console.log("See you later, " + firstName + "!");
+
+// let firstName = prompt("What is your first name?");
+//     // Now firstName should correspond to whatever the user typed!
+// console.log("Hi, " + firstName + "!");
+// console.log("How are you doing, " + firstName + "?");
+// console.log("See you later, " + firstName + "!");
+
+// let phrase = 'Matt said, "I haven\'t been to Chile", the other day.';
+// console.log(phrase); 
+
+// let noValue;
+// console.log(noValue); => undefined
+
+// console.log(typeof " ");
+// console.log(typeof 5);
+// console.log(typeof false);
+// console.log(typeof Symbol());
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// let num = 5;
+// console.log(num);
+// console.log(num.toString());
+
+// let validConversion = Number("2");
+// console.log("Is validConversion NaN?", isNaN(validConversion))
+
+// num = 2;
+// console.log(isNaN(num));
+
+// let greeting = "hi";
+// let nothing = 0;
+// console.log(!!greeting);
+// console.log(!!nothing);
+
+
+
+// EXERCISES
+
+let name = "Wayne";
+let dayOfBirth = 30;
+
+var today = 30;
+var today = 50;
+console.log(today);
+
+let favColor = prompt("What is your favorite color");
+console.log("The color of the browser will now change to " + favColor + " because it is your favorite color");
+
+let famQuotes = 'William B. Smith said, "Calculus is the powerful tool invented by the wit of man."'
+console.log(famQuotes);
+
+// Null signifies an intentional absence of data.
+// Undefined occurs when a variable is not assigned a value.
+
+// NaN stands short of 'Not a Number'
+console.log(typeof NaN); // NaN is a number
+
+let think; // it is undefined
