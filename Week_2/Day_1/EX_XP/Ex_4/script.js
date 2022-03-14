9ad9d65115593da8953d85edec9552245f825526
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-14 17:45:40 +0400

    finish exercise xp fpr ex 4

---
+++ Week_2/Day_1/EX_XP/Ex_4/script.js
@@ -0,0 +1,20 @@
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a + b);
+// Prediction: 55
+// Actual: 55
+
+a = 2;
+
+console.log(a + b);
+// Prediction: 23
+// Actual: 23
+
+// 3. undefined
+
+console.log(3 + 4 + '5');
+// Prediction: 75
+// Actual: 75
+
