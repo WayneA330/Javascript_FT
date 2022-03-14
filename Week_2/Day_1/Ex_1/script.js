da6e8f74915bac5eda2b07eaab4cb8658ac3f0e5
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-14 14:13:28 +0400

    ex 1 for day 1 js

---
+++ Week_2/Day_1/Ex_1/script.js
@@ -0,0 +1,6 @@
+let addressNumber = 9;
+let addressStreet = 'Morcellement Lonrho, Highlands, Phoenix';
+let country = 'Mauritius';
+
+let globalAddress = "I live in" + " " + addressNumber + " " + addressStreet + " " + country;
+console.log(globalAddress);
