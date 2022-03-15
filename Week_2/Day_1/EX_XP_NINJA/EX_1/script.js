e5087a70bfeadc6a6f535e452d1fcba10da77332
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-15 20:53:39 +0400

    need to finish 4Q for ex 1

---
+++ Week_2/Day_1/EX_XP_NINJA/EX_1/script.js
@@ -0,0 +1,29 @@
+console.log(5 >= 1);
+// Answer: true
+
+console.log(0 === 1);
+// Answer: false
+
+console.log(4 <= 1);
+// Answer: false
+
+console.log(1 != 1);
+// Answer: false
+
+console.log("A" > "B");
+// Answer: 
+
+console.log("B" > "C");
+// Answer: 
+
+console.log("a" > "A");
+// Answer: 
+
+console.log("b" > "A");
+// Answer: 
+
+console.log(true === false);
+// Answer: false
+
+console.log(true != true);
+// Answer: false
