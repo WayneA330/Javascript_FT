5f9ef6bc971e372c3179350135413c6841979aa3
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-15 16:08:19 +0400

    finsh exercise 3

---
+++ Week_2/Day_1/EX_XP_GOLD/Ex_3/script.js
@@ -0,0 +1,9 @@
+num1 = prompt('Please enter the first number');
+num1 = Number(num1);
+console.log(num1);
+
+num2 = prompt('Please enter the second number');
+num2 = Number(num2);
+console.log(num1);
+
+alert(num1 + num2);
