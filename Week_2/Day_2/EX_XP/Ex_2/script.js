1b1cfe0e7932a7f654e83f94976155c169800eab
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-16 23:05:37 +0400

    finish

---
+++ Week_2/Day_2/EX_XP/Ex_2/script.js
@@ -0,0 +1,13 @@
+newDog = "Chihuahua";
+
+console.log(newDog.length);
+
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+
+if (newDog === "Chihuahua") {
+    console.log("I love Chihuahuas, it's my favorite dog breed");
+}
+else {
+    console.log("I don't care, I prefer cats");
+}
