56b047fc877d4b48e10c6ad60edcf56230d50517
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-16 14:33:07 +0400

    finished exercise

---
+++ Week_2/Day_2/Ex_1_Object/script.js
@@ -0,0 +1,23 @@
+let obj_credentials = {
+    username: 'Henry',
+    password: 'lucky'
+}
+
+let database_2 = [obj_credentials];
+
+let newsfeed = [
+    {
+        username: 'username1',
+        timeline: 'timeline1'
+    },
+    {
+        username: 'username2',
+        timeline: 'timeline2'
+    },
+    {
+        username: 'username3',
+        timeline: 'timeline3'
+    },
+]
+
+console.log(database_2);
