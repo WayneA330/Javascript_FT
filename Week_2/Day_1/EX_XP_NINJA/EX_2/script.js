3be15726142c78c788394e1a0102027a04d135d6
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-16 19:14:25 +0400

    finished

--- Week_2/Day_1/EX_XP_NINJA/EX_2/script.js
+++ Week_2/Day_1/EX_XP_NINJA/EX_2/script.js
@@ -1,3 +1,15 @@
-let num = [];
-num[num.length] = prompt('Please enter a string of numbers');
-console.log(num);
+num = prompt('Please enter a string of numbers');
+num = num.split(',');
+
+
+num1 = Number(num[0].trim());
+console.log(num1);
+num2 = Number(num[1].trim());
+console.log(num2);
+num3 = Number(num[2].trim());
+console.log(num3);
+num4 = Number(num[3].trim());
+console.log(num4);
+
+sum = num1 + num2 + num3 + num4;
+console.log(sum);
