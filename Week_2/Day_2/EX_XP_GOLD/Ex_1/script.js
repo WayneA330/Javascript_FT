a8024ceea2a30de7561332e4105ed8af412f1e71
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-17 08:43:43 +0400

    finished exercise 1

---
+++ Week_2/Day_2/EX_XP_GOLD/Ex_1/script.js
@@ -0,0 +1,16 @@
+let language = prompt('Which language do you speak?');
+language = language.toLowerCase();
+
+switch (language) {
+    case 'french':
+        alert('Bonjour');
+        break;
+    case 'english':
+        alert('Hello');
+        break;
+    case 'hebrew':
+        alert('Shalom');
+        break;
+    default:
+        alert('01110011 01101111 01110010 01110010 01111001');
+}
