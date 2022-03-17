5417031a8ed874c0f95f0548d9276f20e5229884
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-17 10:12:50 +0400

    finished exercise

---
+++ Week_2/Day_2/EX_XP_GOLD/Ex_3/script.js
@@ -0,0 +1,29 @@
+let verb = prompt('Please enter a verb:');
+verb = verb.trim(); // use TRIM!!!!!
+let verbLength = verb.length;
+
+if (verb.length >= 3) {
+    if (!verb.endsWith('ing')){
+        
+        if (verb.endsWith('n')) {
+            verb += 'n'; // or verb = verb + 'n';
+        }
+        else if (verb.endsWith('m')) {
+            verb += 'm'
+        }
+
+        verb += 'ing';
+    }
+    
+    else {
+        verb = verb + 'ly'; // or verb += 'ly';
+    }
+}
+console.log(verb);
+    
+
+
+
+// (verb.length >= 3 && verb.endsWith('ing')) {
+//     console.log(verb + 'ly');
+// }
