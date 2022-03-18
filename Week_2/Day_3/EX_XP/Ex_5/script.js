1625d8416ee961773f6530998e7b69989cd13079
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-18 10:10:32 +0400

    finish

---
+++ Week_2/Day_3/EX_XP/Ex_5/script.js
@@ -0,0 +1,14 @@
+family =  {
+    brother: 'Mathias',
+    sister: 'Sophie',
+    mother: 'Annick',
+    father: 'Coco'
+}
+
+for (let i in family) {
+    console.log(i);
+}
+
+for (let i in family) {
+    console.log(family[i]);
+}
