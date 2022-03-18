b030432ec1dab1d983b43b8e5526fc6fca117a6f
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-18 07:58:35 +0400

    small edit

--- Week_2/Day_3/EX_XP/Ex_2/script.js
+++ Week_2/Day_3/EX_XP/Ex_2/script.js
@@ -1,7 +1,14 @@
 colors = ["blue", "green", "red", "yellow", "orange"];
+position = ['st', 'nd', 'rd', 'th', 'th'];
 
 for (let idx in colors) {
     let pos = Number(idx)+1; // need to convert to number because index is a string
 
     console.log('My #' + pos + ' choice is ' + colors[idx]);
+}
+
+for (let i in colors && position) {
+    let pos = Number(i)+1;
+
+    console.log('My ' + pos + position[i] + " choice is " + colors[i]);
 }
