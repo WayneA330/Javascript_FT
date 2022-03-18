732cad3c8683e4c33769b94aa37e972d92033c4b
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-18 14:11:31 +0400

    finish

---
+++ Week_2/Day_4/EX_XP/Ex_1/script.js
@@ -0,0 +1,13 @@
+// Part 1
+function infoAboutMe() {
+    console.log('Hi my name is Wayne. I am 18 years old. I am a huge fan of Liverpool FC and I love coding.');
+}
+infoAboutMe();
+
+// Part 2
+function infoAboutPerson(personName, personAge, personFavoriteColor) {
+    console.log('Your name is ' + personName + ' , you are ' + personAge + ' , and your favorite color is ' + personFavoriteColor + '.');
+}
+
+infoAboutPerson("David", 45, "blue");
+infoAboutPerson("Josh", 12, "yellow");
