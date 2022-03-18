ac3ba17be981ef20db43e8f74592612550ebb634
WayneA330 <waynecelestin.a3@gmail.com>
2022-03-18 09:57:08 +0400

    finish

---
+++ Week_2/Day_3/EX_XP/Ex_3/script.js
@@ -0,0 +1,21 @@
+// // While
+// let input1 = prompt('Please enter a number');
+// console.log(typeof input1);
+// input1 = Number(input1);
+// console.log(input1);
+//  // While
+// while(input1 < 10) {
+//     input1 = prompt('Please enter a number greater than 10');
+//     input1 = Number(input1);
+// }
+
+// Do While
+let input2 = "";
+
+do {
+    input2 = prompt('Please enter a number greater than 10');
+    input2 = Number(input2);
+}
+while(input2 < 10);
+
+
