d79f07799ee8b3fca8c383c58a7c1bb4ccac2b25
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-21 11:19:05 +0400

    finish

--- Week_2/Day_4/EX_XP/Ex_3/script.js
+++ Week_2/Day_4/EX_XP/Ex_3/script.js
@@ -1,12 +1,16 @@
  function isDivisible() {
-     
+    let total = 0;
     for (let i = 0; i <= 500; i++) {
-        console.log(i);
+        // console.log(i);
 
+        remainder = i % 23;
+        if (remainder === 0) {
+            console.log(i);
+        }
         
-        
+        total += i;
     }
-
+    console.log(total);
 
 }
 
