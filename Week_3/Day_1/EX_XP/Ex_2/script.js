90366a3da7579cf1c46ddd4525cec1887acc8334
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-22 12:02:45 +0400

    finish

--- Week_3/Day_1/EX_XP/Ex_2/script.js
+++ Week_3/Day_1/EX_XP/Ex_2/script.js
@@ -4,15 +4,23 @@ li_edit[0].lastElementChild.innerText = 'Richard';
 li_edit[0].firstElementChild.innerText = 'Wayne';
 li_edit[1].firstElementChild.innerText = 'Wayne';
 
+// For the first UL
 let li_new = document.createElement('li');
 let text = document.createTextNode('Hey students');
 li_new.appendChild(text);
 li_edit[0].appendChild(li_new);
-li_edit[1].appendChild(li_new);
 
-let secondLI = li_edit[1].childNodes[2];
-li_edit[1].removeChild(secondLI); // need help
+// For the second UL
+let li_new_2 = document.createElement('li');
+let text_2 = document.createTextNode('Hey students');
+li_new_2.appendChild(text_2);
+li_edit[1].appendChild(li_new_2);
+
+let secondLI = li_edit[1].children[1];
+console.log(secondLI);
+li_edit[1].removeChild(secondLI);
 
 let ul_list = document.querySelectorAll('ul');
-ul_list.classList.add('student_list')
-
+ul_list[0].classList.add('student_list');
+ul_list[1].classList.add('student_list');
+ 
