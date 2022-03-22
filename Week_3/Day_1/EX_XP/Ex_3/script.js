3668e160c802384abf162f6571e9ad340143dba7
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-22 09:09:46 +0400

    finish

---
+++ Week_3/Day_1/EX_XP/Ex_3/script.js
@@ -0,0 +1,19 @@
+let div_edit = document.getElementsByTagName('div');
+console.log(div_edit[0]);
+div_edit[0].style.backgroundColor = 'lightBlue';
+div_edit[0].style.padding = '15px';
+
+let ul_list = document.getElementsByTagName('ul')[0];
+let li_remove = document.getElementsByTagName('li');
+console.log(li_remove[0]);
+ul_list.removeChild(li_remove[0]);
+
+console.log(li_remove[0]);
+li_remove[0].style.border = '1px solid black';
+
+font_body = document.body;
+font_body.style.fontSize = '20px';
+
+if (div_edit[0].style.backgroundColor = 'lightBlue') {
+    alert('Hello x and y');
+}
