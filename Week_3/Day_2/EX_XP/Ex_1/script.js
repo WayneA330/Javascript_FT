c36a2dcc1632a22c621fff09a60aefe781ad51ec
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-23 22:18:41 +0400

    finish

---
+++ Week_3/Day_2/EX_XP/Ex_1/script.js
@@ -0,0 +1,38 @@
+let body = document.body;
+
+let h1 = document.getElementsByTagName('h1')[0];
+console.log(h1);
+
+let article = document.getElementsByTagName('article')[0];
+let remove_p = document.querySelectorAll('p')[3];
+article.removeChild(remove_p);
+
+let h2 = document.getElementsByTagName('h2')[0];
+h2.addEventListener('click', function (ev) {
+    h2.style.backgroundColor = 'red';
+});
+
+let h3 = document.getElementsByTagName('h3')[0];
+h3.addEventListener('click', function (ev) {
+    h3.style.display = 'none';
+});
+
+let button_bold = document.getElementById('btn');
+button_bold.addEventListener('click', function(ev) {
+    body.style.fontWeight = 'bold';
+});
+
+h1.addEventListener('mouseover', function (ev) {
+    let random = Math.floor(Math.random() * 101);
+    // console.log(random);
+    h1.style.fontSize = random
+})
+
+let fade_out_p = document.querySelectorAll('p')[1];
+fade_out_p.addEventListener('mouseover', function (ev) {
+    fade_out_p.style.visibility = 'hidden';
+    fade_out_p.style.opacity = '0';
+    fade_out_p.style.transition = 'visibility 0s 1s, opacity 1s linear';
+})
+
+
