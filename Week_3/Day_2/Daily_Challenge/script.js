0fa658150a0f8271ef988bf28a23a2b863b02311
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-24 10:21:29 +0400

    starter files

---
+++ Week_3/Day_2/Daily_Challenge/script.js
@@ -0,0 +1,9 @@
+let btn_lib = document.getElementById('lib-button');
+
+required_att = document.getElement
+
+
+btn_lib.addEventListener('click', (ev) => {
+    let noun = document.getElementById('noun');
+    
+})
