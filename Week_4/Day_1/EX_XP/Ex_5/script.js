7a44b35f87cf4ea33e8dc2e2163f3c7dd12be514
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-03-28 18:46:52 +0400

    samll edit

--- Week_4/Day_1/EX_XP/Ex_5/script.js
+++ Week_4/Day_1/EX_XP/Ex_5/script.js
@@ -2,5 +2,5 @@ let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 let ordinal = ["st","nd","rd", 'th', 'th', 'th', 'th'];
 
 color.forEach((value, idx) => {
-    console.log(`${idx + 1}${ordinal[idx]} is ${color[idx]}`);
+    console.log(`${idx + 1}${ordinal[idx]} is ${value}`);
 });
