9c626c3f55c031ddc9e2db44329dcdb8bd11c805
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-04-05 14:54:53 +0400

    finish exercise

---
+++ Week_5/Day_1/EX_XP/Ex_3/script.js
@@ -0,0 +1,29 @@
+let marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+}
+
+let json_string = JSON.stringify(marioGame);
+console.log(json_string);
+
+let json_string2 = JSON.stringify(marioGame, null, 4);
+console.log(json_string2);
