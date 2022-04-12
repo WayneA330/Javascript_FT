41aa9c79cdd59b962f26ddbb03077a93707377f2
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-04-12 09:21:55 +0400

    finish

---
+++ Week_6/Day_1/EX_XP/customers.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO customers (first_name, last_name)
+-- VALUES('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'), ('Trevor', 'Green'), ('Melanie', 'Johnson');
+
+-- SELECT * FROM customers WHERE last_name = 'Smith';
+
+-- SELECT * FROM customers WHERE last_name = 'Jones';
+
+SELECT * FROM customers WHERE first_name != 'Scott';
+
+-- SELECT * FROM customers;
---
+++ Week_6/Day_1/EX_XP/items.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO items (items, price)
+-- VALUES('Small Desk', 100),('Large Desk', 300), ('Fan', 80);
+
+-- SELECT * FROM items;
+
+-- SELECT * FROM items WHERE price > 80;
+
+SELECT * FROM items WHERE price <= 300;
+
+-- had to delete some data this is why the id is starting on 7
