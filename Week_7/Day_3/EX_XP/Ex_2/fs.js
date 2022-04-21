bd06fec6887ae74913bd07bbad2f25eff0e28e19
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-04-21 21:34:20 +0400

    finish

---
+++ Week_7/Day_3/EX_XP/Ex_2/fs.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+
+// fs.writeFile('test.txt', 'Some dummy text', function(err) {
+//     if(err) {
+//         console.error(err);
+//     }
+//     else {
+//         console.log('Operation Complete');
+//     }
+// });
+
+// fs.appendFile('test.txt', '\n' + 'More more dummy text1', function(err) {
+//     if(err) {
+//         console.log(err);
+//         return
+//     }
+// });
+
+// fs.unlink('test.txt', function (err) {
+//     console.log('write operation complete.');
+// });
+
+// fs.readFile('test.txt', 'utf-8', (err, data) =>{
+//     if (err) {
+//         console.error(err);
+//         return
+//     }
+//     console.log(data);
+// });
