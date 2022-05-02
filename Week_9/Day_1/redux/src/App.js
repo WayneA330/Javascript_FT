751ef16855ed6c34abe2da7b2f05faec5a7408e9
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-05-02 19:44:21 +0400

    finish for intro redux

---
+++ Week_9/Day_1/redux/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Lesson from './Lesson';
+import { createStore } from 'redux';
+import reducer from "./reducers";
+
+const initialState = { subject: "Javascript" };
+const store = createStore(reducer, initialState);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Lesson subject={store.getState().subject} />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ Week_9/Day_1/redux/src/reducers/index.js
@@ -0,0 +1,7 @@
+// export a simple function
+// the reducer take the argument state
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state) => {
+    // always return the NEW STATE of your application
+    return state
+}
