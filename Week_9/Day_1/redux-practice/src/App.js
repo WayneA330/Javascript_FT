168ae063585477cb22f24ee4d1121e1901f4afff
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-05-03 17:13:05 +0400

    finish actions

---
+++ Week_9/Day_1/redux-practice/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Day from './Day';
+
+//import the store from store/index.js file
+import { store } from "./store";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* we call the <Day> Component */}
+        <Day weekday={store.getState().weekday} />
+      </div>
+    )
+  }
+}
+
+export default App;
